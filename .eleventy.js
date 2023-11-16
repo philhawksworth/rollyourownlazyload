dfc12715de0a59218d39a1c23b44d3a493c9924f
Phil Hawksworth <phil@netlify.com>
2023-11-16 17:20:37 +0000

    scss, js, and image CDN updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,45 @@
-module.exports = function(config) {
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const sass = require("sass");
+
+
+module.exports = function(eleventyConfig) {
 
   // Layout aliases can make templates more portable
-  config.addLayoutAlias('default', 'layouts/base.njk');
+  eleventyConfig.addLayoutAlias('default', 'layouts/base.njk');
 
   // syntax highlighting plugin
-  const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
-  config.addPlugin(syntaxHighlightPlugin, {
+  eleventyConfig.addPlugin(syntaxHighlight, {
     templateFormats: "md"
   });
 
+  // Sass pipeline
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: function(contents, includePath) {
+      let includePaths = [this.config.dir.includes];
+      return () => {
+        let ret = sass.renderSync({
+          file: includePath,
+          includePaths,
+          data: contents,
+          outputStyle: "compressed"
+        });
+        return ret.css.toString("utf8");
+      }
+    }
+  });
+  
 
-  // minify the html output when building for realz
-  if(process.env.ELEVENTY_ENV == 'prod') {
-    config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
-  }
 
   // A responsive image helper using Netlify Large Media - image transformation
-  config.addShortcode("picture", require("./src/utils/picture.js"));
+  eleventyConfig.addShortcode("picture", require("./src/utils/picture.js"));
   // A lazy loading image helper using Netlify Large Media - image transformation
-  config.addShortcode("lazypicture", require("./src/utils/lazy-picture.js"));
+  eleventyConfig.addShortcode("lazypicture", require("./src/utils/lazy-picture.js"));
 
   // pass some assets right through
-  config.addPassthroughCopy("./src/site/images");
+  eleventyConfig.addPassthroughCopy("./src/site/images");
+  eleventyConfig.addPassthroughCopy("./src/site/js/*.js");
 
 
   return {
